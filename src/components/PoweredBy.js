98f37572d3646512da30a9c41fdc3ca213ac5d13
Gerry Yang <gerry04y@gmail.com>
2025-01-15 15:49:02 +0000

    Powered By

--- src/components/PoweredBy.js
+++ src/components/PoweredBy.js
@@ -1,24 +1,22 @@
-import Card from "./Card";
-import POWERED_BY from "../content/poweredBy";
+import ReactLogo from "../assets/react-logo-1.png";
+import Javascriptlogo from "../assets/javascript-logo.png";
+import CanvaLogo from "../assets/canva-logo.png";
+import WordpressLogo from "../assets/wordpress-logo-1.png";
+
+const LOGOS = [WordpressLogo, ReactLogo, Javascriptlogo, CanvaLogo];
 
 function PoweredBy() {
   return (
-    <div className="flex flex-col justify-center items-center py-16 md:py-18 px-5">
-      <h1 className="text-primary py-4 md:py-12">Powered By</h1>
-      <div className="md:grid md:grid-cols-3 md:gap-4 md:justify-between lg:max-w-[70%] md:max-w-[80%]">
-        {POWERED_BY.map(
-          ({ title, image, link, description, dates, points, skills }) => (
-            <Card
-              image={image}
-              link={link}
-              title={title}
-              dates={dates}
-              description={description}
-              points={points}
-              skills={skills}
-            />
-          )
-        )}
+    <div className="flex flex-col justify-center items-center py-12">
+      <h1 className="text-primary pt-4 pb-8 md:pb-12">Powered By</h1>
+      <div className="grid grid-cols-2 gap-6 max-w-[80%] mb-8 md:flex md:flex-row md:mb-12">
+        {LOGOS.map((logo) => (
+          <img
+            src={logo}
+            alt="Powered By Logo"
+            className="md:max-h-44"
+          />
+        ))}
       </div>
     </div>
   );
