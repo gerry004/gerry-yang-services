7143cd07f44ca3bb69d4a024b6f3b6a328df61d0
Gerry Yang <gerry04y@gmail.com>
2025-01-15 21:07:09 +0000

    Copywrite 2025

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <div className="bg-black p-2">
       <p className="text-white text-center">
-        @Copyright 2023. Handcrafted by Gerry Yang with React JS and Tailwind
+        @Copyright 2025. Handcrafted by Gerry Yang with React JS and Tailwind
         CSS
       </p>
     </div>
